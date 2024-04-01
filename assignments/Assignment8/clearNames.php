4960e362557a8fef0cc001750d9bf59f1bcc9fdf
iKhouri <khfountain@outlook.com>
2024-03-31 22:17:39 -0400

    updates

--- assignments/Assignment8/clearNames.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'Db_conn.php'; 
-
-$db = new DB();
-$db->query("TRUNCATE TABLE names");
-$db->execute();
-?>
--- assignments/Assignment8/starter_scripts/php/clearNames.php
+++ assignments/Assignment8/starter_scripts/php/clearNames.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'Db_conn.php'; 
 
 $db = new DB();
 $db->query("TRUNCATE TABLE names");
